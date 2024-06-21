22b8b105843494f5a883c33b11b2fafb2f9eba92
lijojs <lijo.js@iroidtechnologies.com>
2024-06-22 00:18:35 +0530

    first commit

---
+++ theme/home.php
@@ -0,0 +1,20 @@
+<?php
+/*
+*
+* Template Name: home Page 
+*
+*/
+
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+
+        <h1 class="text-red-600 text-9xl">Lijo js</h1>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
---
+++ theme/single.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package developer_lijo
+ */
+
+get_header();
+?>
+
+	<section id="primary">
+		<main id="main">
+		<h1 class="text-red-600 text-9xl">Lijo js</h1>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content/content', 'single' );
+
+				if ( is_singular( 'post' ) ) {
+					// Previous/next post navigation.
+					the_post_navigation(
+						array(
+							'next_text' => '<span aria-hidden="true">' . __( 'Next Post', 'developer-lijo' ) . '</span> ' .
+								'<span class="sr-only">' . __( 'Next post:', 'developer-lijo' ) . '</span> <br/>' .
+								'<span>%title</span>',
+							'prev_text' => '<span aria-hidden="true">' . __( 'Previous Post', 'developer-lijo' ) . '</span> ' .
+								'<span class="sr-only">' . __( 'Previous post:', 'developer-lijo' ) . '</span> <br/>' .
+								'<span>%title</span>',
+						)
+					);
+				}
+
+				// If comments are open, or we have at least one comment, load
+				// the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+				// End the loop.
+			endwhile;
+			?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php
+get_footer();
